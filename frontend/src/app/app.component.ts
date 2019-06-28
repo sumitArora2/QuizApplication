4698ded21e0c7402bd7f3fd93ec61388f4649705
Sumit Arora <arora.sumit857@gmail.com>
2019-06-28 10:58:51 +0530

    ll

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
-  title = 'frontend Changes';
+  title = 'frontend';
 }
