bd2a06c2688e2610480261020b3e4e7d33e1f4fa
Sumit Arora <arora.sumit857@gmail.com>
2019-06-28 10:33:53 +0530

    add components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PrincipalDashboardComponent } from './principal-dashboard/principal-dashboard.component';
+import { StudentDashboardComponent } from './student-dashboard/student-dashboard.component';
+import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent,
+    FooterComponent,
+    PrincipalDashboardComponent,
+    StudentDashboardComponent,
+    TeacherDashboardComponent
   ],
   imports: [
     BrowserModule,
