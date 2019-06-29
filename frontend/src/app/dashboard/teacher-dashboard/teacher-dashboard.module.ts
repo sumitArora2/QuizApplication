628a66d058460cc546817613d2d5ab168c2af476
divesh malhotra <m.divesh98@gmail.com>
2019-06-29 10:00:30 +0530

    fjierfj

--- frontend/src/app/dashboard/teacher-dashboard/teacher-dashboard.module.ts
+++ frontend/src/app/dashboard/teacher-dashboard/teacher-dashboard.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeacherHomeComponent } from './teacher-home/teacher-home.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [TeacherHomeComponent],
   imports: [
-    CommonModule
+    CommonModule,RouterModule
   ]
 })
 export class TeacherDashboardModule { }
