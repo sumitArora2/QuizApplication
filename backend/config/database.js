7bee5b85690fb6acae0d90f3abdc48a1b7ec6531
Sumit Arora <arora.sumit857@gmail.com>
2019-07-06 14:23:07 +0530

    online database connection with mlab

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,5 @@
 module.exports={
-    database:'mongodb://localhost:27017/QuizDB',
+    // database:'mongodb://localhost:27017/QuizDB',
+    database:'mongodb+srv://sumit:sumit123@quizdb-kpopc.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'yoursecret'
 };
