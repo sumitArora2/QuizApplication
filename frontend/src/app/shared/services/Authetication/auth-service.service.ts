920771294a9c821f04a9dbe27f84c06166c4757f
Chanchal Jangir <chanchaljangir9@gmail.com>
2019-07-08 10:01:21 +0530

    pull changes

--- frontend/src/app/shared/services/Authetication/auth-service.service.ts
+++ frontend/src/app/shared/services/Authetication/auth-service.service.ts
@@ -1,11 +1,7 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import {HttpClient,HttpHeaders} from '@angular/common/http';
 import {map} from 'rxjs/Operators';
-=======
-import { HttpClient } from '@angular/common/http';
 
->>>>>>> 7ddbfc32d42483d023984bdc344cae36da74e7bf
 @Injectable({
   providedIn: 'root'
 })
@@ -16,15 +12,12 @@ export class AuthServiceService {
   // uri='http://localhost:4000';
   constructor(private http:HttpClient) { }
 
-<<<<<<< HEAD
   postLogin(userauth){
     let headers=new HttpHeaders();
     headers.append('Content-Type','application/json');
     return this.http.post('http://localhost:3000/api/authenticate',userauth,{headers:headers})
   .pipe(map(res=>res));
   }
-=======
-  constructor(http:HttpClient) { }
->>>>>>> 7ddbfc32d42483d023984bdc344cae36da74e7bf
+ 
 }
  
