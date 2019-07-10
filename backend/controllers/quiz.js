94effd5265af215423cca446ecb13b72db726861
Sumit Arora <arora.sumit857@gmail.com>
2019-07-10 16:57:33 +0530

    Quiz backend updated

--- backend/controllers/quiz.js
+++ backend/controllers/quiz.js
@@ -1,32 +1,42 @@
-const Quiz=require('../models/quiz');
+const Quiz = require("../models/quiz");
 
-
-
-async function getQuestions(req,res){
-try{
-
-}catch(error){
-throw error
-}
-}
-
-module.exports={
-      addQuestion : async(req,res) => {
-        try{
-         let quiz=new Quiz(req.body);
-         let result=await quiz.save();
-         res.send(result);
-        }catch(error){
-        throw(error);
-        }
-    },  
-    getQuestions : async(req,res)=>{
-        try{
-            // let quiz=new Quiz();
-           let result=await Quiz.find();
-           res.status(200).send({ message: 'all questions', result: result });
-        }catch(error){
-            throw(error);
-        }
+module.exports = {
+  addQuestion: async (req, res) => {
+    try {
+      let quiz = new Quiz(req.body);
+      let result = await quiz.save();
+      res.send(result);
+    } catch (error) {
+      throw error;
     }
+  },
+  getQuestions: async (req, res) => {
+    try {
+      // let quiz=new Quiz();
+      let result = await Quiz.find();
+      res.status(200).send({ message: "all questions", result: result });
+    } catch (error) {
+      throw error;
+    }
+  },
+  addMoreQuestion: async (req, res) => {
+    var newQuestion = req.body;
+    var SubId = req.params.id;
+    try {
+      let result = await Quiz.updateOne({ _id: SubId }, { $push:{Questions: newQuestion} });
+      res.status(200).send({ message: "adding more question", result: result });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  DeleteQuestion: async(req, res) => {
+    var SubId = req.params.id;
+    var QuesId =req.params.id; 
+    try {
+      let result = await Quiz.deleteOne({ _id: QuesId }, { $push:{Questions: newQuestion} });
+      res.status(200).send({ message: "adding more question", result: result });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
