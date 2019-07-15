15e313eb9195d0d1a75ecca7c145a863f2d031d0
Divesh98 <m.divesh98@gmail.com>
2019-07-15 10:25:08 +0530

    login signup nd complaint form

--- frontend/src/app/dashboard/principal-dashboard/complaints/complaints.component.ts
+++ frontend/src/app/dashboard/principal-dashboard/complaints/complaints.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthServiceService } from 'src/app/shared/services/Authetication/auth-service.service';
+import { FormGroup,Validators,FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 @Component({
   selector: 'app-complaints',
   templateUrl: './complaints.component.html',
   styleUrls: ['./complaints.component.css']
 })
 export class ComplaintsComponent implements OnInit {
+  complaintForm:FormGroup;
 
-  constructor() { }
+  constructor(private authService:AuthServiceService,private router:Router,private flashService:FlashMessagesService) { }
 
   ngOnInit() {
-  }
+    this.complaintForm = new FormGroup({
 
+      'firstname' : new FormControl(null, [Validators.required,Validators.maxLength(25)]),
+      'lastname': new FormControl(null,[Validators.required,Validators.maxLength(25)]),
+      'email' : new FormControl(null,[Validators.required,Validators.email]),
+      'message' : new FormControl(null,[Validators.required,Validators.maxLength(50)])
+  });
+
+}
 }
--- frontend/src/app/dashboard/principal-dashboard/principal-dashboard.module.ts
+++ frontend/src/app/dashboard/principal-dashboard/principal-dashboard.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { PrincipalHomeComponent } from './principal-home/principal-home.component';
 import { RouterModule } from '@angular/router';
 import { ComplaintsComponent } from './complaints/complaints.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PrincipalHomeComponent, ComplaintsComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,FormsModule
   ]
 })
 export class PrincipalDashboardModule { }
