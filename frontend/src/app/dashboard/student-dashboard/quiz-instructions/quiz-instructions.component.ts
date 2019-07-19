87950ecae875a08f18985c6f9062a3895accd583
Sumit Arora <arora.sumit857@gmail.com>
2019-07-19 18:03:54 +0530

    Questions template changes

--- frontend/src/app/dashboard/student-dashboard/quiz-instructions/quiz-instructions.component.ts
+++ frontend/src/app/dashboard/student-dashboard/quiz-instructions/quiz-instructions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-instructions',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz-instructions.component.css']
 })
 export class QuizInstructionsComponent implements OnInit {
-
-  constructor() { }
+  timeLeft= 60;
+  interval;
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    this.timeLeft=60;
+    this.interval = setInterval(() => {
+      if (this.timeLeft > 0) {
+        this.timeLeft--;
+      } else {
+        this.timeLeft = 60;
+      }
+    }, 1000);
   }
-
+  StartQuiz(){
+    this.router.navigate(['/studentQuiz']);
+  }
 }
