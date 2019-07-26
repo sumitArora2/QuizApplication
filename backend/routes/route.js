ccaaf76aa16a6e259ebed28e33296fa1f79df300
Chanchal Jangir <chanchaljangir9@gmail.com>
2019-07-26 13:13:04 +0530

    add more quiz in existing chapters

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -74,7 +74,7 @@ router.route('/question').post(quiz.addQuestion);
 router.route('/question').get(quiz.getQuestions);
 router.route('/marks/:userId/:chapterId').post(quiz.addMarks);
 router.route('/marks/:userId').get(quiz.getMarks);
-router.route('/addmorequestion/chapterId').patch(quiz.addMoreQuestion);
+router.route('/addmorequestion/:chapterId').patch(quiz.addMoreQuestion);
 // router.route('/question/:classid').patch(quiz.addMoreQuestion);
 // router.route('/question/:subjectId/:questionId').patch(quiz.DeleteQuestion); 
 
